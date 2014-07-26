a40b8c291d436412eced16641b5bb5963fc2e13f
Danny Whalen <daniel.r.whalen@gmail.com>
2014-07-26 11:57:05 -0700

    Add Seattle Building Permits

--- lib/spy_glass/registry.rb
+++ lib/spy_glass/registry.rb
@@ -1,4 +1,51 @@
 require 'spy_glass/clients'
+require 'money'
+
+#########################################
+# Seattle Building Permits
+#########################################
+opts = {
+  path: '/seattle-building-permits',
+  cache: SpyGlass::Caches::Memory.new(expires_in: 1200),
+  source: 'http://data.seattle.gov/resource/mags-97de?'+Rack::Utils.build_query({
+    '$limit' => 250,
+    '$order' => 'application_date DESC',
+    '$where' => <<-WHERE.oneline
+      status = 'Application Accepted' AND
+      longitude IS NOT NULL AND
+      latitude IS NOT NULL AND
+      application_date IS NOT NULL AND
+      category IS NOT NULL AND
+      address IS NOT NULL AND
+      permit_type IS NOT NULL
+    WHERE
+  })
+}
+
+SpyGlass::Registry << SpyGlass::Clients::Socrata.new(opts) do |collection|
+  features = collection.map do |item|
+    title = <<-TITLE.oneline
+      Hi! A building permit for #{item['category'].downcase} #{item['permit_type'].downcase} has been submitted near you at #{item['address']}.
+      The proposed value is #{Money.new(item['value'].to_i*100, 'USD').format}.
+      Find out more at #{item['permit_and_complaint_status_url']['url']}.
+    TITLE
+
+    {
+      'id' => item['application_permit_number'],
+      'type' => 'Feature',
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [
+          item['longitude'].to_f,
+          item['latitude'].to_f
+        ]
+      },
+      'properties' => item.merge('title' => title)
+    }
+  end
+
+  {'type' => 'FeatureCollection', 'features' => features}
+end
 
 #########################################
 # Seattle Code Case Violations
@@ -7,12 +54,13 @@ opts = {
   path: '/seattle-code-violation-cases',
   cache: SpyGlass::Caches::Memory.new(expires_in: 1200),
   source: 'http://data.seattle.gov/resource/dk8m-pdjf?'+Rack::Utils.build_query({
-    '$limit' => 100,
+    '$limit' => 250,
     '$order' => 'date_case_created DESC',
     '$where' => <<-WHERE.oneline
       longitude IS NOT NULL AND
       latitude IS NOT NULL AND
       date_case_created IS NOT NULL AND
+      address IS NOT NULL AND
       case_group IS NOT NULL
     WHERE
   })
