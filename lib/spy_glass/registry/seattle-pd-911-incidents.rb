5bc1bd01bc46e32f8dd73468afc27e91470f9469
Danny Whalen <daniel.r.whalen@gmail.com>
2014-08-13 14:12:47 -0700

    Additional cleanup

--- lib/spy_glass/registry/seattle-pd-911-incidents.rb
+++ lib/spy_glass/registry/seattle-pd-911-incidents.rb
@@ -2,7 +2,7 @@ require 'spy_glass/registry'
 
 time_zone = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
 title_template = ERB.new(<<-ERB.oneline)
-  A 911 incident has occurred near you at <%= block_location %>.
+  A 911 incident occurred near you at <%= block_location %>.
   It was described as "<%= description %>" and was cleared at <%= clearance_time %>.
   <% if duration %>The incident remained open for <%= duration %>.<% end %>
 ERB
