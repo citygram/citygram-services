b2c958907bc3ec2eb3085cec64fbe493e1b29c6f
Danny Whalen <daniel.r.whalen@gmail.com>
2014-09-16 10:39:37 -0700

    Add Charlotte Land Development projects

--- lib/spy_glass/registry.rb
+++ lib/spy_glass/registry.rb
@@ -3,11 +3,28 @@ require 'active_support/time'
 require 'action_view'
 require 'core_ext/string'
 require 'erb'
+require 'json'
+require 'logger'
 require 'money'
 require 'rack/utils'
+require 'sequel'
 require 'spy_glass/client'
 
 module SpyGlass
+  module Utils
+    SPC2LatLng = <<-SQL.freeze
+      SELECT ST_AsGeoJSON(ST_Transform(ST_SetSRID(ST_MakePoint(?::numeric, ?::numeric), 3359), 4326)) AS latlng
+    SQL
+
+    DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
+    DB.loggers << Logger.new(STDOUT)
+
+    def self.convert_spc_to_latlng(x, y)
+      geojson = DB.dataset.with_sql(SPC2LatLng, x, y).get
+      JSON.parse(geojson)['coordinates']
+    end
+  end
+
   Registry = []
   Salutations = [
     'Hi!',
---
+++ lib/spy_glass/registry/clt-land-development.rb
@@ -0,0 +1,39 @@
+require 'spy_glass/registry'
+
+sql = <<-SQL.gsub(/(\s{2,}|\n)/, " ").strip
+SELECT "9f57ae4b-fc62-4ae3-895d-aece0e759b5c".*
+FROM "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"
+WHERE "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"."X_COORD" IS NOT NULL
+AND "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"."Y_COORD" IS NOT NULL
+AND "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"."PROJECTDESCRIPTION" IS NOT NULL
+AND "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"."OWNERNAME" IS NOT NULL
+ORDER BY "9f57ae4b-fc62-4ae3-895d-aece0e759b5c"."RECORDOPENDATE" DESC
+LIMIT 100
+SQL
+
+opts = {
+  path: '/clt-land-development',
+  cache: SpyGlass::Cache::Memory.new(expires_in: 2400),
+  source: 'http://www.civicdata.com/api/action/datastore_search_sql?'+Rack::Utils.build_query(sql: sql)
+}
+
+SpyGlass::Registry << SpyGlass::Client::JSON.new(opts) do |body|
+  features = body['result']['records'].map do |record|
+    lat, lng = SpyGlass::Utils.convert_spc_to_latlng(record['X_COORD'], record['Y_COORD'])
+
+    {
+      'id' => record['PROJECTNUMBER'],
+      'type' => 'Feature',
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [lat,lng]
+      },
+      'properties' => record.merge(
+        'title' => record['PROJECTDESCRIPTION'],
+        'url' => record['URL']
+      )
+    }
+  end
+
+  { 'type' => 'FeatureCollection', 'features' => features }
+end
