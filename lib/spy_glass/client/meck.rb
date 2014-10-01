cfd07dbe079c0e5cfa010a359ff2a4719169db25
Danny Whalen <daniel.r.whalen@gmail.com>
2014-10-01 11:41:28 -0700

    Fix namespacing

--- lib/spy_glass/client/meck.rb
+++ lib/spy_glass/client/meck.rb
@@ -13,8 +13,8 @@ module SpyGlass
     class Meck < Base
       DEFAULT_OPTIONS = {
         content_type: 'application/json',
-        parser: JSON.method(:parse),
-        generator: JSON.method(:pretty_generate)
+        parser: ::JSON.method(:parse),
+        generator: ::JSON.method(:pretty_generate)
       }
 
       def initialize(attrs, &block)
