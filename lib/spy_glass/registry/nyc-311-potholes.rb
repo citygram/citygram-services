1863f6b9cbffe5e2f642b96a7becf8247f1dd0eb
Dirk Kelly <talk+dirkkelly@beta.nyc>
2015-10-17 01:24:39 -0400

    removed some refactors

--- lib/spy_glass/registry/nyc-311-potholes.rb
+++ lib/spy_glass/registry/nyc-311-potholes.rb
@@ -20,25 +20,22 @@ opts = {
   source: 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?'+ Rack::Utils.build_query(query)
 }
 
+time_zone = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
   features = collection.map do |item|
     time = Time.iso8601(item['created_date']).in_time_zone(time_zone).strftime("%m/%d  %I:%M %p")
     city = item['city'].capitalize
-    intersection_street_1 = item['intersection_street_1'].titleize
-    intersection_street_2 = item['intersection_street_2'].titleize
-    cross_street_1 = item['cross_street_1'].titleize
-    cross_street_2 = item['cross_street_2'].titleize
-    street = item['street_name'].titleize
+
     title =
       case item['address_type']
       when 'ADDRESS'
         "#{time} - A new 311 case has been opened at #{item['incident_address'].titleize} in #{city}."
       when 'INTERSECTION'
-        "#{time} - A new 311 case has been opened at the intersection of #{intersection_street_1} and #{intersection_street_2} in #{city}."
+        "#{time} - A new 311 case has been opened at the intersection of #{intersection_street_1 = item['intersection_street_1'].titleize} and #{intersection_street_2 = item['intersection_street_2'].titleize} in #{city}."
       when 'BLOCKFACE'
-        "#{time} - A new 311 case has been opened on #{street}, between #{cross_street_1} and #{cross_street_2} in #{city}."
+        "#{time} - A new 311 case has been opened on #{street}, between #{cross_street_2 = item['cross_street_2'].titleize} and #{cross_street_1 = item['cross_street_1'].titleize} in #{city}."
       else
-        "#{time} - A new 311 case has been opened on #{street} in #{city}."
+        "#{time} - A new 311 case has been opened on #{item['street_name'].titleize} in #{city}."
       end
     title << " The complaint type is #{item['complaint_type'].downcase} - #{item['descriptor']} and the assigned agency is #{item['agency']}."
 
