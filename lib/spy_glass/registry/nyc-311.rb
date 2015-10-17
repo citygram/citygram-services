0b023b593109a0bc720944ba53cccf391556ef84
Dirk Kelly <talk+dirkkelly@beta.nyc>
2015-10-17 01:40:36 -0400

    fix street name

--- lib/spy_glass/registry/nyc-311.rb
+++ lib/spy_glass/registry/nyc-311.rb
@@ -31,7 +31,7 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
       when 'INTERSECTION'
         "#{time} - A new 311 case has been opened at the intersection of #{item['intersection_street_1'].titleize} and #{item['intersection_street_2'].titleize} in #{city}."
       when 'BLOCKFACE'
-        "#{time} - A new 311 case has been opened on #{street}, between #{item['cross_street_2'].titleize} and #{item['cross_street_1'].titleize} in #{city}."
+        "#{time} - A new 311 case has been opened on #{item['street_name'].titleize}, between #{item['cross_street_2'].titleize} and #{item['cross_street_1'].titleize} in #{city}."
       else
         "#{time} - A new 311 case has been opened on #{item['street_name'].titleize} in #{city}."
       end
