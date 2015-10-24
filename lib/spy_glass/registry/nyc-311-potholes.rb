7791b32a9debc90ce95f64f9038a0191e15e365c
Dirk Kelly <dirkkelly@users.noreply.github.com>
2015-10-24 12:55:12 -0400

    like-potholes

--- lib/spy_glass/registry/nyc-311-potholes.rb
+++ lib/spy_glass/registry/nyc-311-potholes.rb
@@ -7,7 +7,7 @@ query = {
     created_date >= '#{7.days.ago.iso8601}' AND
     longitude IS NOT NULL AND
     latitude IS NOT NULL AND
-    descriptor = 'Pothole' AND
+    descriptor LIKE 'Pothole%' AND
     unique_key IS NOT NULL
   WHERE
 }
