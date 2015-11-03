f6d163199ad3e242c46311a0d365ee269328eb7a
Bryce Kerley <bkerley@brycekerley.net>
2015-11-02 20:11:56 -0500

    start miami 311

---
+++ lib/spy_glass/registry/miami-311.rb
@@ -0,0 +1,60 @@
+require 'spy_glass/registry'
+
+time_zone = ActiveSupport::TimeZone["Eastern Time (US & Canada)"]
+
+opts = {
+  path: '/miami-311',
+  cache: SpyGlass::Cache::Memory.new(expires_in: 300),
+  source: 'https://opendata.miamidade.gov/resource/dj6j-qg5t.json?'+Rack::Utils.build_query({
+    '$limit' => 1000,
+    '$order' => 'ticket_created_date_time DESC',
+    '$where' => <<-WHERE.oneline
+      ticket_created_date_time >= '#{7.days.ago.iso8601}' AND
+      longitude IS NOT NULL AND
+      latitude IS NOT NULL AND
+      ticket_id IS NOT NULL
+    WHERE
+  })
+}
+
+SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
+  features = collection.map do |item|
+    time = Time.iso8601(item['ticket_created_date_time']).in_time_zone(time_zone)
+
+    city = item['city']
+    title =
+      #case item['address_type']
+      #when 'ADDRESS'
+        "#{Time.iso8601(item['ticket_created_date_time']).strftime("%m/%d  %I:%M %p")} - A new 311 case has been opened at #{item['street_address'].titleize}."
+        #when 'INTERSECTION'
+        #intersection_street_1 = item['intersection_street_1']
+        #intersection_street_2 = item['intersection_street_2']
+        #"#{Time.iso8601(item['created_date']).strftime("%m/%d  %I:%M %p")} - A new 311 case has been opened at the intersection of #{intersection_street_1.titleize} and #{intersection_street_2.titleize} in #{city.capitalize}."
+        #when 'BLOCKFACE'
+        #cross_street_1 = item['cross_street_1']
+        #cross_street_2 = item['cross_street_2']
+        #street = item['street_name']
+        #"#{Time.iso8601(item['created_date']).strftime("%m/%d  %I:%M %p")} - A new 311 case has been opened on #{street.titleize}, between #{cross_street_1.titleize} and #{cross_street_2.titleize} in #{city.capitalize}."
+        #else
+        #"#{Time.iso8601(item['created_date']).strftime("%m/%d  %I:%M %p")} - A new 311 case has been opened on #{item['street_name']} in #{city}."
+        #end
+
+    title << " The complaint type is #{item['issue_type'].downcase} and the assigned agency is #{item['case_owner']}."
+
+    {
+      'id' => item['ticket_id'],
+      'type' => 'Feature',
+      'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [
+          item['longitude'].to_f,
+          item['latitude'].to_f
+        ]
+      },
+      'properties' => item.merge('title' => title)
+    }
+  end
+
+  {'type' => 'FeatureCollection', 'features' => features}
+end
+
