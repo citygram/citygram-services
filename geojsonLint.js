4526fae8a21fdfacff0b7c55b2fd57b45b799711
Jim Van Fleet <jim.van.fleet@lattapartners.com>
2015-12-21 12:21:09 -0500

    Running geojsonhint under test

--- geojsonLint.js
+++ geojsonLint.js
@@ -1,8 +1,34 @@
 /*! geojsonLint v0.0.0 - MIT license */
 'use strict';
 
-var geojsonLint = function () {
-  // your code goes here
+var geojsonhint = require('geojsonhint');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var geojsonLint = {
+  all: function (path) {
+    return geojsonhint(path);
+  },
+
+  testPath: function (path) {
+    var results = request('http://localhost:5000/'+path, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        debugger;
+        return geojsonhint(body);
+      } else {
+        return false
+      }
+    });
+    return results;
+  },
+
+  findEndpoints: function () {
+    var endpointsPath = path.join(__dirname, 'lib/spy_glass/registry');
+    return fs.readdirSync(endpointsPath).map(function(elt){
+      return elt.substr(0, elt.length-3)
+    });
+  }
 }
 
 if ( typeof module !== "undefined" ) {
--- spec/geojsonLint.js
+++ spec/geojsonLint.js
@@ -7,17 +7,43 @@ describe('geojsonLint()', function () {
   'use strict';
 
   it('exists', function () {
-    expect(geojsonLint).to.be.a('function');
+    expect(geojsonLint).to.be.a('Object');
 
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+});
+
+describe('all()', function () {
+  it('finds all endpoints to test', function () {
+    expect(geojsonLint.all).to.be.a('function')
+  });
+});
+
+describe('testPath()', function() {
+  it("should be a function", function () {
+    expect(geojsonLint.testPath).to.be.a('function')
   });
 
-  it('does something else', function () {
-    expect(true).to.equal(false);
+  it("should test a path", function () {
+    var firstPath = geojsonLint.findEndpoints()[0];
+    expect(geojsonLint.testPath(firstPath)).to.be.true
   });
 
-  // Add more assertions here
+});
+
+describe('findEndpoints()', function () {
+  it('finds all endpoints to test', function () {
+    expect(geojsonLint.findEndpoints).to.be.a('function')
+  });
+
+  it('returns more than a dozen values', function () {
+    expect(geojsonLint.findEndpoints()).to.be.an('Array')
+    expect(geojsonLint.findEndpoints().length).to.be.above(12)
+  });
+
+  it('strips RB from filenames', function () {
+    var firstPath = geojsonLint.findEndpoints()[0];
+    expect(firstPath).to.be.an('String');
+    expect(firstPath).not.to.match(/rb/);
+  });
 });
