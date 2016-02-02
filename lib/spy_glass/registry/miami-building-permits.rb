ae9bb629502c3047e067c5025dd37defc6838397
Bryce Kerley <bkerley@brycekerley.net>
2016-02-01 20:11:06 -0500

    building permits sorted by PERMIT-ISSUED-DATE

--- lib/spy_glass/registry/miami-building-permits.rb
+++ lib/spy_glass/registry/miami-building-permits.rb
@@ -1,26 +1,30 @@
+# coding: utf-8
 require 'spy_glass/registry'
 
 time_zone = ActiveSupport::TimeZone["Eastern Time (US & Canada)"]
 
+source_base = 'https://opendata.miamidade.gov/resource/kw55-e2dj.json'
+options = {
+  '$limit' => 1000,
+  '$order' => '"PERMIT-ISSUED-DATE" DESC',
+  '$where' => <<-WHERE.oneline
+    "PERMIT-ISSUED-DATE" >= '#{7.days.ago.iso8601}' AND
+    Location IS NOT NULL
+  WHERE
+}
+
+source_url = source_base + '?' + Rack::Utils.build_query(options)
+
 opts = {
   path: '/miami-building-permits',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'https://opendata.miamidade.gov/resource/kw55-e2dj.json?'+Rack::Utils.build_query({
-    '$limit' => 1000,
-    '$order' => 'ticket_created_date_time DESC',
-    '$where' => <<-WHERE.oneline
-      ticket_created_date_time >= '#{7.days.ago.iso8601}' AND
-      longitude IS NOT NULL AND
-      latitude IS NOT NULL AND
-      street_address IS NOT NULL AND
-      issue_type IS NOT NULL AND
-      ticket_id IS NOT NULL
-    WHERE
-  })
+  source: source_url
 }
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
+raise collection.inspect
   features = collection.map do |item|
+
     time = Time.iso8601(item[0]['ticket_created_date_time']).in_time_zone(time_zone)
 
     city = item['city']
@@ -28,9 +32,9 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
       "#{Time.iso8601(item['ticket_created_date_time']).strftime("%m/%d  %I:%M %p")} - A new building permit has been issued at #{item['street_address']} to #{item['owner_name']}."
 
     # title << " The complaint type is #{item['issue_type']} and the assigned agency is #{item['case_owner'].gsub('_', ' ')}."
-    location_regex = /\((-?[\d.]+)°,\s(-?[\d.]+)°\)/ 
+    location_regex = /\((-?[\d.]+)°,\s(-?[\d.]+)°\)/
     location_match = location_regex.match item['location']
-    
+
 
     {
       'id' => item['ticket_id'],
@@ -48,4 +52,3 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
 
   {'type' => 'FeatureCollection', 'features' => features}
 end
-
