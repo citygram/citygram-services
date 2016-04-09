94ec243b8b8f468a968b1ddb3d462538ff78e59a
Jim Van Fleet <jim@jimvanfleet.com>
2016-04-09 16:38:46 -0400

    Bringing historic district reviews back online

--- lib/spy_glass/registry/clt-historic-district-reviews.rb
+++ lib/spy_glass/registry/clt-historic-district-reviews.rb
@@ -3,20 +3,22 @@ require 'spy_glass/registry'
 opts = {
   path: '/clt-historic-district-reviews',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'http://maps.ci.charlotte.nc.us/arcgis/rest/services/PLN/CharlotteHistoricDistrictCases/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&gdbVersion=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&orderByFields=AppDate+desc&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&f=pjson'
+  source: 'http://clt.charlotte.opendata.arcgis.com/datasets/694f78a23c194a0bae1847a7ff71618d_0.geojson'
 }
 
 SpyGlass::Registry << SpyGlass::Client::JSON.new(opts) do |esri_formatted|
   features = esri_formatted['features'].map do |feature|
-    attributes = feature['attributes']
+    attributes = feature['properties']
     unique_id = attributes['OBJECTID']
-    next if feature['geometry'].nil?
-    coordinates = feature['geometry']['rings']
-    project_description = attributes['ProjDesc'].strip
-    project_address = attributes['ProjAddr']
-    unique_title = 'New Request for ' +
-      project_description + ' at ' +
-      project_address + '. For more, call (704) 336-2205 or visit http://j.mp/clt-chd-reviews'
+    next if feature['geometry'].nil? || attributes['ProjAddr'].nil?
+    desc_phrase = ''
+    if attributes['ProjDesc']
+      project_description = attributes['ProjDesc'].strip
+      desc_phrase = "for #{project_description} "
+    end
+    project_address = attributes['ProjAddr'].strip
+    unique_title = "New Request #{desc_phrase}at #{project_address}. " + 
+      "For more, call (704) 336-2205 or visit http://j.mp/clt-chd-reviews"
 
     {
       'type' => 'Feature',
@@ -25,7 +27,7 @@ SpyGlass::Registry << SpyGlass::Client::JSON.new(opts) do |esri_formatted|
         'title' => unique_title,
         'url' => 'http://charmeck.org/city/charlotte/planning/HistoricDistricts/Pages/ByDistrict.aspx'
       ),
-      'geometry' => { 'type' => 'Polygon', 'coordinates' => coordinates }
+      'geometry' => feature['geometry']
     }
   end
 
