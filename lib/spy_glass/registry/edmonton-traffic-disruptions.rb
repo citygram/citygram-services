0d0cecf2e53db0ac04d133af2d854eefcc1ed745
Jim Van Fleet <jim.van.fleet@levvel.io>
2017-09-26 15:14:54 -0400

    Fixing some issues with the traffic disruption submission

--- lib/spy_glass/registry/edmonton-traffic-disruptions.rb
+++ lib/spy_glass/registry/edmonton-traffic-disruptions.rb
@@ -3,24 +3,29 @@ require 'spy_glass/registry'
 time_zone = ActiveSupport::TimeZone["Mountain Time (US & Canada)"]
 
 opts = {
-  path: '/edmonton-traffic-disruptions.rb',
+  path: '/edmonton-traffic-disruptions',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'https://data.edmonton.ca/resource/ju4q-wijd.json' + Rack::Utils.build_query({
+  source: 'https://data.edmonton.ca/resource/87ck-293k.json?' + Rack::Utils.build_query({
     '$order' => 'starting_date DESC',
-    '$limit' => 10,
-    '$where' => " location IS NOT NULL"
+    '$limit' => 100,
+    '$where' => "location IS NOT NULL"
   })
 }
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
-  puts("collection: #{collection}")
   features = collection.map do |record|
     title = "#{record['details']}."
       {
         'id'=> record['disruption_number'],
         'type'=> 'Feature',
         'properties' => record.merge('title' => title),
-        'geometry' => record['location']
+        'geometry' => {
+          'type' => 'Point',
+          'coordinates' => [
+            record['location']['longitude'].to_f,
+            record['location']['latitude'].to_f
+          ]
+        }
       }
   end
 
