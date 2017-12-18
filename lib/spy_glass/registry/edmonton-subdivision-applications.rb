c91acdb6f7ddd88b52738090f806b0c4a332464b
Jim Van Fleet <jim.van.fleet@levvel.io>
2017-12-18 09:55:39 -0500

    Refining subdivision applications, ready for go-live

--- lib/spy_glass/registry/edmonton-subdivision-applications.rb
+++ lib/spy_glass/registry/edmonton-subdivision-applications.rb
@@ -6,15 +6,19 @@ opts = {
   path: '/edmonton-subdivision-applications',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
   source: 'https://data.edmonton.ca/resource/ezyy-ern8?' + Rack::Utils.build_query({
-#    '$order' => 'permit_date DESC',
-    '$limit' => 10,
-    '$where' => " status == 'In Review'"
+    '$order' => 'filenumber DESC',
+    '$limit' => 20,
   })
 }
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
   features = collection.map do |record|
-    title = "#{record['legaldesc']} has been filed to recognize #{record['address']} as a subdivision within the #{record['nhood']} neighborhood."
+    title = nil
+    if record['status'] == "In Review"
+       title = "Parcel(s) of land at #{record['address']} are currently in review for a subdivision application within the #{record['nhood']} neighborhood.  The file number is #{record['filenumber']} for #{record['legaldesc']}"
+    else
+       title = "Parcel(s) of land at #{record['address']} have been approved for a subdivision application within the #{record['nhood']} neighborhood.  The file number is #{record['filenumber']} for #{record['legaldesc']}"
+    end
       {
         'id'=> record['filenumber'],
         'type'=> 'Feature',
@@ -22,6 +26,5 @@ SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
         'geometry' => record['the_geom']
       }
   end
-
   {'type' => 'FeatureCollection', 'features' => features}
 end
