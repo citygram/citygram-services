37690523c02294c5086d00cb7d676bfb46f3caa5
jsomoza <somoza@ualberta.ca>
2018-05-07 12:28:43 -0600

    updated building permits and created dev permit template

--- lib/spy_glass/registry/edmonton-development-permits.rb
+++ lib/spy_glass/registry/edmonton-development-permits.rb
@@ -1,11 +1,13 @@
 require 'spy_glass/registry'
 
+# code reuse from edmonton-building-permits.rb, to be modified when dev permit dataset is available
+
 time_zone = ActiveSupport::TimeZone["Mountain Time (US & Canada)"]
 
 opts = {
-  path: '/edmonton-building-permits',
+  path: '/edmonton-development-permits',
   cache: SpyGlass::Cache::Memory.new(expires_in: 300),
-  source: 'https://data.edmonton.ca/resource/rwuh-apwg?' + Rack::Utils.build_query({
+  source: 'https://data.edmonton.ca/resource/rwuh-apwg?' + Rack::Utils.build_query({ # TODO: change to dev permit dataset when available
     '$order' => 'permit_date DESC',
     '$limit' => 10,
     '$where' => " latitude IS NOT NULL" +
@@ -15,7 +17,8 @@ opts = {
 
 SpyGlass::Registry << SpyGlass::Client::Socrata.new(opts) do |collection|
   features = collection.map do |record|
-    title = "A building permit was issued on #{record['issue_date']} at #{record['address']} as City File # #{record['permit_number']}. #{record['job_description']}. " 
+    # TODO: possibly modify
+    title = "A development permit #{record['permit_class']} was approved on #{record['issue_date']} at #{record['address']} as City File # #{record['permit_number']}. #{record['job_description']}."
       {
         'id'=> record['permit_number'],
         'type'=> 'Feature',
