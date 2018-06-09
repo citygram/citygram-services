de349ac0b65285d404f71ddcf5248ec893ba7ca9
Michael duPont <michael@mdupont.com>
2018-06-09 17:33:28 -0400

    Implemented Orlando voting center

--- lib/spy_glass/registry/orlando-voting-centers.rb
+++ lib/spy_glass/registry/orlando-voting-centers.rb
@@ -11,30 +11,52 @@ opts = {
 SpyGlass::Registry << SpyGlass::Client::JSON.new(opts) do |body|
   # For each election
   # Generate most recent notification if within one day
+
+  # Days out to be search from today
+
+  days_out = body['days_out']
+
+  # Stash the today date
+  today = Date.current
+
   # Create map of all precincts
 
-  elections = body.map do |election|
-
-    # Days out to be search from today.
-    # days_out = [0, 1, 3, 7, 14]
-    days_out = election['days_out']
-
-    # Stash the today date.
-    today = Date.current
+  elections = body['elections'].map do |election|
 
     # Target date to compare with current day.
-    election_date = election['elections']['date']
+    election_date = Date.parse election['date']
 
     # If difference between today and Election Date is equal to one into the
     # Days Out, get the Geomatries.
-    if days_out.include?(today - election_date)
+    diff = Integer(election_date - today)
+    if days_out.include?diff
 
-      election['geoms']['geometry']['geometry']
+      # Create the election/date alert for each precinct
+      features = body['geoms'].reject{|x| x['properties']['voting_center'].nil?}.map do |geom|
 
+        # IDs must be unique per message
+        id = "#{today}|#{election['name']}|#{diff}|#{geom['properties']['precinct']}"
+
+        # Title is the text message content
+        title = "The #{election['name']} is "
+        if diff == 0
+          title << 'today'
+        elsif diff == 1
+          title << 'tomorrow'
+        else
+          title << "in #{diff} days"
+        end
+        center = geom['properties']['voting_center'].split.map(&:capitalize).join(' ')
+        title << " on #{election_date}. Your polling place is the #{center}"
+
+        {
+          'id' => id,
+          'type' => 'Feature',
+          'properties' => {'title' => title},
+          'geometry' => geom['geometry']
+        }
+      end
     end
-
   end
-
-  { 'type' => 'ElectionCollection', 'elections' => elections }
-
+  { 'type' => 'FeatureCollection', 'features' => elections.flatten! }
 end
